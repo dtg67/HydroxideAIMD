09a97b1d138f5000491cb8c8045f628b9275a39a
diego <dgomez1@tulane.edu>
2021-09-23 19:41:21 -0500

    Adding main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import re
+import math
+import argparse
 import pandas as pd
-import numpy as np
 
-print("Here is a github test")
+#### COMMANDLINE ARGUMENT PASSING ####
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file", action = "store",
+                    help = "CP2K trajectory file")
+parser.add_argument("-L", "--length", action = "store",
+                    help = "Simulation box length in angstroms")
+parser.add_argument("-N", )
+args = parser.parse_args()
+l = float(args.length)
+cp2k = args.file
+
